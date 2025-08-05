55a9b7fae0ce5b791a742059c5ec6b1f42ff1cee
Irving Felipe Lozarate <irving@twyzle.com>
2025-08-05 11:44:21 -0700

    chore: update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,17 +5,31 @@ export default defineConfig([
     entry: ['src/index.ts'],
     format: 'esm',
     dts: true,
-    outExtension: () => ({ js: '.mjs' }),
     splitting: false,
     clean: true,
+    outExtension: () => ({ js: '.mjs' }),
+    external: ['lodash-es'],
   },
   {
     entry: ['src/index.ts'],
     format: 'cjs',
+    splitting: false,
+    clean: false,
     outExtension: () => ({ js: '.cjs' }),
-    define: {
-      'lodash-es': 'require("lodash")' // key line
+    external: ['lodash'], // required for CJS
+    esbuildOptions(options) {
+      // 👇 rewrite imports of 'lodash-es' to 'lodash'
+      options.plugins = [
+        {
+          name: 'alias-lodash-es-to-lodash',
+          setup(build) {
+            build.onResolve({ filter: /^lodash-es$/ }, args => ({
+              path: 'lodash',
+              external: true,
+            }))
+          },
+        },
+      ]
     },
-    clean: false
-  }
+  },
 ])
